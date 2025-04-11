b26b4101291567bc17a51b63d94377ef193b7887
XuchenSun <sunxuchen1991@gmail.com>
2025-04-11 00:42:33 -0400

    update stack

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,30 +1,44 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
+    SiVisualstudiocode,
+    SiPostman,
+    SiSlack,
+    SiVercel,
+    SiMacos, SiIntellijidea, SiChromecast, SiSplunk, SiFirefox, SiLinux, SiNotepadplusplus, SiVim, SiWebstorm, SiJira,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiIntellijidea />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
+
+        <Col xs={4} md={2} className="tech-icons">
+            <SiNotepadplusplus />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiVim />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiJira/>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <SiSplunk/>
+        </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
+        <SiFirefox />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
+        <SiWebstorm />
       </Col>
     </Row>
   );
