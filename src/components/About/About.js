94978edc9c4ea1a25c85f16b55c2fb4413c15596
XuchenSun <sunxuchen1991@gmail.com>
2025-04-15 21:50:35 -0400

    udate about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,7 +22,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+              <strong className="purple">Software Engineer</strong> |<strong className="purple"> MASc</strong> | <strong className="purple">B.Eng</strong>
             </h1>
             <Aboutcard />
           </Col>
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,34 +8,15 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Xuchen Sun </span>
-            from <span className="purple"> Markham, Ontario.</span>
-            <br />
-            A dedicated software developer with 5+ years of experience, MASc in Computer Engineering and B.Eng in Computer Science.
+            Hi, I'm Xuchen Sun, a dedicated Software Engineer based in Markham, Ontario, with <span className="purple">5+ years </span>of experience <span className="purple">designing, building</span>, and <span className="purple">deploying enterprise-level applications</span>. I specialize in full-stack development, Java and React Application, and delivering high-impact software solutions in agile environments.
             <br/>
             <br />
-            Currently employed as a software developer at <span className="purple">TD Canada Trust.</span>
-
+            Currently employed as a software developer at <span className="purple">TD Canada Trust</span>.
             <br />
             <br />
-            Apart from work, some other activities that I love to do!
+            Currently seeking a senior software developer position to leverage extensive experience in software engineering and cloud solutions !
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> <span className="purple">Design </span>minimum viable product!
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> <span className="purple">Develop </span>minimum viable product!
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> <span className="purple">Deliver </span>minimum viable product!
-            </li>
-          </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "We never know until we try!"{" "}
-          </p>
-          <footer className="blockquote-footer">Xuchen</footer>
         </blockquote>
       </Card.Body>
     </Card>
