90ee4cf93335a8fb32e5603ee08128f3c9fa310a
XuchenSun <sunxuchen1991@gmail.com>
2025-04-15 23:23:00 -0400

    udpate project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,18 +15,18 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My Previous <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are a few projects I've worked on previously.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              title="Game Image Design Bases on GAN and CNN"
+              description="Applying GAN(Generative Adversarial Network) and CNN(convolutional neural network) to generate images of games."
               ghLink="https://github.com/soumyajit4419/Chatify"
               demoLink="https://chatify-49.web.app/"
             />
